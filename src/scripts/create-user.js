311553b1a69f62c8874453c3591e5e60c0f09961
Kelly <devteam@afripointgroup.com>
2024-12-13 22:48:00 +0100

    updated bases url

--- src/scripts/create-user.js
+++ src/scripts/create-user.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',  // Changed to local development server
+  baseURL: 'https://hirely-task-manager-backend.vercel.app/api/users',  // Changed to local development server
   headers: {
     'Content-Type': 'application/json',
   },
